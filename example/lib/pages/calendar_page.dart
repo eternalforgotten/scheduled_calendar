e0646abb8d758350d1f65d9c3da39cd7e2e95fe6
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-09-27 11:54:00 +0300

    example created

---
+++ example/lib/pages/calendar_page.dart
@@ -0,0 +1,11 @@
+import 'package:calendar_builder/calendar_builder.dart';
+import 'package:flutter/material.dart';
+
+class CalendarPage extends StatelessWidget {
+  const CalendarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
