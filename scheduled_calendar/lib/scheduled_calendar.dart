cf0da3b02ace21ff911ac3cff5498d7a92ac3726
eternalforgotten <grechishkin.nikita@mail.ru>
2023-09-27 13:13:13 +0300

    example added

--- scheduled_calendar/lib/scheduled_calendar.dart
+++ scheduled_calendar/lib/scheduled_calendar.dart
@@ -1,7 +1,22 @@
 library scheduled_calendar;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/widgets.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class ScheduledCalendar extends StatefulWidget {
+  const ScheduledCalendar({super.key});
+
+  @override
+  State<ScheduledCalendar> createState() => _ScheduledCalendarState();
+}
+
+class _ScheduledCalendarState extends State<ScheduledCalendar> {
+  @override
+  Widget build(BuildContext context) {
+    return TableCalendar(
+      focusedDay: DateTime.now(),
+      firstDay: DateTime(2023, 9, 25),
+      lastDay: DateTime(2023, 10, 27),
+    );
+  }
 }
