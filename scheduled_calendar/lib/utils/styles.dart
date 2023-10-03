4f10fc4767277d5f41eb31e007dc4cbb89a311a7
ANNA BEZUGLOVA <annawos17@gmail.comA>
2023-10-03 15:20:26 +0300

    client booking card created

--- scheduled_calendar/lib/utils/styles.dart
+++ scheduled_calendar/lib/utils/styles.dart
@@ -122,13 +122,85 @@ class ScheduleInscriptionStyle {
   const ScheduleInscriptionStyle({
     this.textStyle = const TextStyle(
       fontSize: 12,
-      height: 16/12,
+      height: 16 / 12,
     ),
     this.instructionText = 'Расписание на дальнейшую неделю будет доступно',
     this.instructionTextStyle = const TextStyle(
       color: Color(0xFF5C5B5F),
     ),
     this.dateTextStyle = const TextStyle(
+      color: Color(0xFFEFD23C),
+    ),
+  });
+}
+
+class ClientBookingCardStyle {
+  final Decoration cardDecoration;
+  final TextStyle dateTextStyle;
+  final String instructionText;
+  final TextStyle instructionTextStyle;
+  final String buttonText;
+  final TextStyle buttonTextStyle;
+  final TextStyle inactiveButtonTextStyle;
+  final TextStyle timeSlotTextStyle;
+  final Decoration timeSlotDecoration;
+  final TextStyle selectedTimeSlotTextStyle;
+  final Decoration selectedTimeSlotDecoration;
+
+  const ClientBookingCardStyle({
+    this.cardDecoration = const BoxDecoration(
+      borderRadius: BorderRadius.all(Radius.circular(15)),
+      color: Color(0xFF1C1C1F),
+    ),
+    this.dateTextStyle = const TextStyle(
+      fontSize: 18,
+      fontWeight: FontWeight.w600,
+      color: Color(0xFFF6F5F8),
+      height: 24.51 / 18,
+    ),
+    this.instructionText = 'Выберите время на этот день',
+    this.instructionTextStyle = const TextStyle(
+      fontSize: 12,
+      fontWeight: FontWeight.w400,
+      color: Color(0xFF5C5B5F),
+      height: 16 / 12,
+    ),
+    this.buttonText = 'Выбрать',
+    this.buttonTextStyle = const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+      color: Color(0xFFEFD23C),
+      height: 22 / 14,
+    ),
+    this.inactiveButtonTextStyle = const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w600,
+      color: Color(0xFF5C5B5F),
+      height: 22 / 14,
+    ),
+    this.timeSlotTextStyle = const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: Colors.white,
+      height: 22 / 13,
+    ),
+    this.timeSlotDecoration = const BoxDecoration(
+      border: Border.fromBorderSide(
+        BorderSide(
+          width: 1,
+          color: Color(0xFFEFD23C),
+        ),
+      ),
+      borderRadius: BorderRadius.all(Radius.circular(6)),
+    ),
+    this.selectedTimeSlotTextStyle = const TextStyle(
+      fontSize: 14,
+      fontWeight: FontWeight.w400,
+      color: Color(0xFF0A0A0A),
+      height: 22 / 13,
+    ),
+    this.selectedTimeSlotDecoration = const BoxDecoration(
+      borderRadius: BorderRadius.all(Radius.circular(6)),
       color: Color(0xFFEFD23C),
     ),
   });
---
+++ scheduled_calendar/lib/widgets/client_booking_card.dart
@@ -0,0 +1,91 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:scheduled_calendar/utils/styles.dart';
+
+class ClientBookingCard extends StatefulWidget {
+  final DateTime date;
+  final List<DateTime> timeSlots;
+  final ClientBookingCardStyle style;
+  final ValueChanged<DateTime> onClientCardButtonPressed;
+  final String? locale;
+  const ClientBookingCard(
+    this.date, {
+    super.key,
+    required this.timeSlots,
+    this.style = const ClientBookingCardStyle(),
+    required this.onClientCardButtonPressed,
+    this.locale,
+  });
+
+  @override
+  State<ClientBookingCard> createState() => _ClientBookingCardState();
+}
+
+class _ClientBookingCardState extends State<ClientBookingCard> {
+  @override
+  void initState() {
+    super.initState();
+    initializeDateFormatting();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final locale = widget.locale ?? Platform.localeName;
+    final slots = [
+      DateTime(2023, 10, 1, 22, 00),
+      DateTime(2023, 10, 1, 22, 30),
+      DateTime(2023, 10, 1, 23, 00),
+      DateTime(2023, 10, 1, 23, 30),
+      DateTime(2023, 10, 1, 22, 30),
+      DateTime(2023, 10, 1, 22, 30),
+    ];
+    return Container(
+      padding: const EdgeInsets.fromLTRB(37, 22, 37, 25),
+      decoration: widget.style.cardDecoration,
+      child: Column(
+        children: [
+          Text(
+            DateFormat('EEEE, d MMMM', locale).format(widget.date),
+            style: widget.style.dateTextStyle,
+          ),
+          Text(
+            widget.style.instructionText,
+            style: widget.style.instructionTextStyle,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 25),
+            child: GridView.builder(
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 4,
+                childAspectRatio: 62 / 32,
+                mainAxisSpacing: 7,
+                crossAxisSpacing: 7,
+              ),
+              itemBuilder: (context, index) => Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 15,
+                  vertical: 5,
+                ),
+                decoration: widget.style.timeSlotDecoration,
+                child: Text(
+                  DateFormat('Hm', locale).format(slots[index]),
+                ),
+              ),
+            ),
+          ),
+          TextButton(
+            onPressed: () => widget.onClientCardButtonPressed(widget.date),
+            child: Text(
+              widget.style.buttonText,
+              style: widget.style.buttonTextStyle,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
